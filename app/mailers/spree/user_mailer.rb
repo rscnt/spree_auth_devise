3529fba886cca582627c3e9d76ad80ef1b27efa2
Alexander Sosa <alexandersosa2204@gmail.com>
2015-03-06 14:08:51 -0600

    Adding confirmation_intruction.html.erb

--- app/mailers/spree/user_mailer.rb
+++ app/mailers/spree/user_mailer.rb
@@ -2,17 +2,16 @@ module Spree
   class UserMailer < BaseMailer
     def reset_password_instructions(user, token, *args)
       @edit_password_reset_url = spree.edit_spree_user_password_url(:reset_password_token => token, :host => Spree::Store.current.url)
-
-      content = mail(to: user.email, from: from_address, subject: Spree::Store.current.name + ' ' + I18n.t(:subject, :scope => [:devise, :mailer, :reset_password_instructions]))
-      content = content.html_part.body
+      content = mail().html_part.body
 
       resetpassword_instructions(user, token, content)
     end
 
     def confirmation_instructions(user, token, opts={})
       @confirmation_url = spree.spree_user_confirmation_url(:confirmation_token => token, :host => Spree::Store.current.url)
-
-      mail to: user.email, from: from_address, subject: Spree::Store.current.name + ' ' + I18n.t(:subject, :scope => [:devise, :mailer, :confirmation_instructions])
+      content = mail().html_part.body
+      
+      confirmationinstructions(user, token, content)
     end
   end
 end
